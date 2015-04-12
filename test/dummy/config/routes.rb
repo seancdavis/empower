aa521f89fe01fa0d296363271dca8e558b35d1f2
Sean C Davis <scdavis41@gmail.com>
2015-04-12 09:25:53 -0400

    remove test app and update gemspec

--- test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount Empower::Engine => "/empower"
-end
--- test/empower_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EmpowerTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Empower
-  end
-end
